f5a75fba4b7a171ad1e5be5e0a9c941c09f9ea0b
Peter Reid <peter.d.reid@gmail.com>
2016-03-05 21:52:15 -0500

    License under MIT/Apache-2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+// Copyright 2016 Peter Reid. See the COPYRIGHT file at the top-level
+// directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //! In cryptography, keystreams are sequences of bytes that can be
 //! XORed with a plaintext to create a ciphertext or XORed with a
 //! ciphertext to recover the plaintext. A good keystream is
